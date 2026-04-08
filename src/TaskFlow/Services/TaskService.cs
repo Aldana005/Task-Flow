38cf94f723feea85ef0c5ec18aab31ef6a6ba18f
GonzaloVallejo-Ausi <gonzalo.vallejo@mi.unc.edu.ar>
2026-04-08 17:21:20 -0300

    Se creo estrutura TaskService

---
+++ src/TaskFlow/Services/TaskService.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TaskFlow.Models;
+
+namespace TaskFlow.Services 
+{
+    public class TaskService 
+    {
+        //lista interna
+
+        private List<TaskItem> _tasks;
+
+        public TaskService() 
+        {
+            _tasks = new List<TaskItem>();
+        }
+
+        //metodo para crear la tarea
+
+        public void CreateTask(string title, string description, string responsible) 
+        {
+            //generar el id incremental
+            int newId;
+
+            if (_tasks.Count > 0)
+            {
+                newId = _tasks.Max(t => t.Id) + 1;
+            }
+            else 
+            {
+                newId = 1;
+            }
+
+            //instanciar TaskItem
+
+            var newTask = new TaskItem
+            {
+                Id = newId,
+                Title = title,
+                Description = description,
+                Responsible = responsible
+            };
+
+            _tasks.Add(newTask);
+            Console.WriteLine($"Tarea '{title}' creada con éxito con el ID: {newId}");
+        } 
+
+    }
+}
